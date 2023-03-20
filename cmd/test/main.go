5085dc65b92a8e6a7a5570d2aa216aa8f45d6ee0
Nicholas Dille <nicholas.dille@mailbox.org>
2023-03-20 17:48:58 +0100

    Merged into cmd/test

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,24 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/nicholasdille/docker-setup/pkg/tool"
+
+	"github.com/regclient/regclient"
+	"github.com/regclient/regclient/types/ref"
+	"github.com/regclient/regclient/types/manifest"
 )
 
 func main() {
+	arch := "x86_64"
+	alt_arch := "amd64"
+	target := "/usr/local"
+
 	tools, err := tool.LoadFromFile("metadata.json")
 	if err != nil {
 		os.Exit(1)
 	}
 
-	//tools.List()
 	tool, err := tools.GetByName("docker")
 	if err != nil {
 		os.Exit(1)
 	}
-	tool.ReplaceVariables("/usr/local", "x86_64", "amd64")
+	tool.ReplaceVariables(target, arch, alt_arch)
 
 	err = tool.GetBinaryStatus()
 	if err != nil {
@@ -33,4 +42,68 @@ func main() {
 	}
 
 	tool.Print()
+
+	if tool.Status.VersionMatches {
+		fmt.Printf("Nothing to do\n")
+		os.Exit(0)
+	}
+
+	image := fmt.Sprintf("ghcr.io/nicholasdille/docker-setup/%s:main", tool.Name)
+
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(10 * time.Second))
+	defer cancel()
+
+	r, err := ref.New(image)
+	if err != nil {
+		return
+	}
+
+	rcOpts := []regclient.Opt{}
+	rcOpts = append(rcOpts, regclient.WithUserAgent("docker-setup"))
+	rcOpts = append(rcOpts, regclient.WithDockerCreds())
+	rc := regclient.New(rcOpts...)
+	defer rc.Close(ctx, r)
+
+	m, err := rc.ManifestGet(ctx, r)
+	if err != nil {
+        fmt.Println(err)
+		return
+	}
+
+	if m.IsList() {
+		fmt.Println("list")
+
+		if mi, ok := m.(manifest.Indexer); ok {
+			manifests, err := mi.GetManifestList()
+			if err != nil {
+				fmt.Println("Error getting manifests")
+				os.Exit(1)
+			}
+			fmt.Printf("Manifest count: %d\n", len(manifests))
+
+			for i, manifest := range manifests {
+				fmt.Printf("Manifest %d Platform %s\n", i, manifest.Platform.Architecture)
+				if manifest.Platform.Architecture == alt_arch {
+					fmt.Printf("Digest of manifest: %s\n", manifest.Digest)
+
+					// TODO: Get manifest
+
+					break
+				}
+			}
+		}
+
+	} else {
+		fmt.Println("no list")
+
+		if mi, ok := m.(manifest.Imager); ok {
+			fmt.Printf("Type of mi: %T", mi)
+			layers, err := mi.GetLayers()
+			if err != nil {
+				fmt.Println("Error getting layers")
+				os.Exit(1)
+			}
+			fmt.Printf("Layer count: %d\n", len(layers))
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/jedib0t/go-pretty/v6 v6.4.6
+	github.com/regclient/regclient v0.4.7
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/cobra v1.6.1
 	github.com/ulikunitz/xz v0.5.11
@@ -11,9 +12,12 @@ require (
 )
 
 require (
+	github.com/docker/libtrust v0.0.0-20160708172513-aabc10ec26b7 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.5.0 // indirect
 )
