da37c9a5ed549b431f07acaaa9e19eaf78f1e3b4
Nicholas Dille <nicholas.dille@mailbox.org>
2023-05-19 15:43:29 +0200

    Fix go build again

--- make/go.mk
+++ make/go.mk
@@ -26,10 +26,9 @@ build: docker-setup
 GO_SOURCES = $(shell find . -type f -name \*.go)
 docker-setup: make/go.mk $(GO_SOURCES)
 	@\
-	docker run --rm \
-	    --mount type=bind,src=$${HOME}/go/pkg/mod,dst=/go/pkg/mod \
-	    --mount type=bind,src=$${HOME}/.cache/go-build,dst=/.cache/go-build \
-		--mount type=bind,src=$${PWD},dst=/src \
+	docker run \
+        --rm \
+	    --mount type=bind,src=$${PWD},dst=/src \
 		--workdir /src \
 		--user $$(id -u):$$(id -g) \
 		--env CGO_ENABLED=0 \
