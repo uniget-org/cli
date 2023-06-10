c35355338ceab7d8d7448be2145adf26873b881b
Nicholas Dille <nicholas.dille@mailbox.org>
2023-06-10 14:49:48 +0200

    Added option to specify go binary

--- make/go.mk
+++ make/go.mk
@@ -1,21 +1,28 @@
 GO_SOURCES = $(shell find . -type f -name \*.go)
 GO_VERSION = $(shell git describe --tags --abbrev=0)
+GO         = go
 
 .PHONY:
 go-info:
 	@echo "GO_VERSION: $(GO_VERSION)"
 
 bin/docker-setup: bin/docker-setup-linux-$(ALT_ARCH)
-	@cp bin/docker-setup-linux-$(ALT_ARCH) bin/docker-setup
+	@\
+	cp bin/docker-setup-linux-$(ALT_ARCH) bin/docker-setup; \
+	cp bin/docker-setup-linux-$(ALT_ARCH) docker-setup
 
 bin/docker-setup-linux-$(ALT_ARCH):bin/docker-setup-linux-%: make/go.mk $(GO_SOURCES) ; $(info $(M) Building docker-setup version $(GO_VERSION) for $(ALT_ARCH)...)
 	@\
 	export GOOS=linux; \
 	export GOARCH=$*; \
 	CGO_ENABLED=0 \
-		go build -buildvcs=false -ldflags "-w -s -X main.version=$(GO_VERSION)" -o bin/docker-setup-$${GOOS}-$${GOARCH} ./cmd/docker-setup
+		$(GO) build \
+			-buildvcs=false \
+			-ldflags "-w -s -X main.version=$(GO_VERSION)" \
+			-o bin/docker-setup-$${GOOS}-$${GOARCH} \
+			./cmd/docker-setup
 
 .PHONY:
 go-deps:
-	@go get -u ./...
-	@go mod tidy
+	@$(GO) get -u ./...
+	@$(GO) mod tidy
