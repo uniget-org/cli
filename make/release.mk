09ffcc6f9d6ea1f0e31676c97bd490329192a21e
Nicholas Dille <nicholas.dille@mailbox.org>
2023-06-28 08:14:16 +0200

    Fixed release make targets

--- make/release.mk
+++ make/release.mk
@@ -5,15 +5,15 @@ LATEST_VERSION := $(shell echo $(LATEST_TAG) | tr -d v)
 patch: \
 		$(HELPER)/var/lib/docker-setup/manifests/semver.json \
 		; $(info $(M) Creating patch release...)
-	@make tag-$$(semver bump patch $(LATEST_VERSION))
+	@make tag--$$(semver bump patch $(LATEST_VERSION))
 
 .PHONY:
 minor: ; $(info $(M) Creating minor release...)
-	@make tag-$$(semver bump minor $(LATEST_VERSION))
+	@make tag--$$(semver bump minor $(LATEST_VERSION))
 
 .PHONY:
 major: ; $(info $(M) Creating major release...)
-	@make tag-$$(semver bump major $(LATEST_VERSION))
+	@make tag--$$(semver bump major $(LATEST_VERSION))
 
 tag--%: ; $(info $(M) Creating tag v$*...)
 	@if git show-ref --tags refs/tags/v$* >/dev/null 2>&1; then \
