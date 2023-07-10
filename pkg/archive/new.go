6db3be3528c0b231d83509bd94383e99167d4e0f
Nicholas Dille <nicholas.dille@mailbox.org>
2023-07-10 16:17:30 +0200

    Fixed handling of hard links

--- pkg/archive/new.go
+++ pkg/archive/new.go
@@ -69,8 +69,24 @@ func ExtractTarGz(gzipStream io.Reader) error {
 				}
 			}
 
+		case tar.TypeLink:
+			log.Tracef("Untarring link %s\n", header.Name)
+			_, err := os.Stat(header.Name)
+			if err == nil {
+				log.Debugf("Link %s already exists\n", header.Name)
+			}
+			if os.IsNotExist(err) {
+				log.Debugf("Target of link %s does not exist\n", header.Name)
+				os.Remove(header.Name)
+
+				err = os.Link(header.Linkname, header.Name)
+				if err != nil {
+					return fmt.Errorf("ExtractTarGz: Link() failed: %s", err.Error())
+				}
+			}
+
 		default:
-			return fmt.Errorf("ExtractTarGz: unknown type for entry %s", header.Name)
+			return fmt.Errorf("ExtractTarGz: unknown type for entry %s: %b", header.Name, header.Typeflag)
 		}
 
 	}
@@ -107,8 +123,38 @@ func ListTarGz(gzipStream io.Reader) ([]string, error) {
 		case tar.TypeSymlink:
 			result = append(result, header.Name+" -> "+header.Linkname)
 
+		case tar.TypeLink:
+			result = append(result, header.Name+" -> "+header.Linkname)
+
+		case tar.TypeChar:
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: TypeChar", header.Name)
+
+		case tar.TypeBlock:
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: TypeBlock", header.Name)
+
+		case tar.TypeFifo:
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: TypeFifo", header.Name)
+
+		case tar.TypeCont:
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: TypeCont", header.Name)
+
+		case tar.TypeXHeader:
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: TypeXHeader", header.Name)
+
+		case tar.TypeXGlobalHeader:
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: TypeXGlobalHeader", header.Name)
+
+		case tar.TypeGNULongLink:
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: TypeGNULongLink", header.Name)
+
+		case tar.TypeGNULongName:
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: TypeGNULongName", header.Name)
+
+		case tar.TypeGNUSparse:
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: TypeGNUSparse", header.Name)
+
 		default:
-			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s", header.Name)
+			return nil, fmt.Errorf("ListTarGz: unknown type in entry %s: %b", header.Name, header.Typeflag)
 		}
 
 	}
