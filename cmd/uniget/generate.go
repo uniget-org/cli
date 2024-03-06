b46c914fd109a3ecdf6fde17712da0ca0346306e
Nicholas Dille <nicholas.dille@mailbox.org>
2024-03-06 20:28:51 +0100

    Version bump for dockerfile frontend

--- cmd/uniget/generate.go
+++ cmd/uniget/generate.go
@@ -28,7 +28,7 @@ var generateCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		var result []string
 
-		result = append(result, "# syntax=docker/dockerfile:1.6.0")
+		result = append(result, "# syntax=docker/dockerfile:1.7.0")
 		result = append(result, fmt.Sprintf("FROM %s", baseImage))
 
 		for _, toolName := range args {
