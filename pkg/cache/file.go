3d2e1955fbdd2c8e37b0dbeba0c8601f4e22644c
Nicholas Dille <nicholas.dille@mailbox.org>
2024-09-27 11:56:53 +0200

    Cache is now configurable

--- pkg/cache/file.go
+++ pkg/cache/file.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/uniget-org/cli/pkg/containers"
+	"github.com/uniget-org/cli/pkg/logging"
 )
 
 type FileCache struct {
@@ -24,6 +25,7 @@ func (c *FileCache) cacheDirectoryExists() bool {
 		return false
 	}
 
+	logging.Tracef("Checking cache directory %s", c.cacheDirectory)
 	_, err := os.Stat(c.cacheDirectory)
 	return !os.IsNotExist(err)
 }
@@ -33,6 +35,7 @@ func (c *FileCache) writeDataToCache(data []byte, ref string) error {
 		return fmt.Errorf("cache directory is not set")
 	}
 
+	logging.Tracef("Writing data to cache for ref %s", ref)
 	err := os.WriteFile(fmt.Sprintf("%s/%s", c.cacheDirectory, ref), data, 0644) // #nosec G306 -- just for testing
 	if err != nil {
 		return fmt.Errorf("failed to write data for ref %s to cache: %s", ref, err)
@@ -45,6 +48,7 @@ func (c *FileCache) checkDataInCache(ref string) bool {
 		return false
 	}
 
+	logging.Tracef("Checking cache for ref %s", ref)
 	_, err := os.Stat(fmt.Sprintf("%s/%s", c.cacheDirectory, ref))
 	return !os.IsNotExist(err)
 }
@@ -54,6 +58,7 @@ func (c *FileCache) readDataFromCache(ref string) ([]byte, error) {
 		return nil, fmt.Errorf("cache directory is not set")
 	}
 
+	logging.Tracef("Reading data from cache for ref %s", ref)
 	data, err := os.ReadFile(fmt.Sprintf("%s/%s", c.cacheDirectory, ref))
 	if err != nil {
 		return nil, fmt.Errorf("failed to read data for ref %s from cache: %s", ref, err)
@@ -64,17 +69,20 @@ func (c *FileCache) readDataFromCache(ref string) ([]byte, error) {
 func (c *FileCache) Get(tool *containers.ToolRef) ([]byte, error) {
 	cacheKey := tool.Key()
 	if !c.checkDataInCache(tool.String()) {
+		logging.Debugf("FileCache: Cache miss for %s", tool.String())
 		layer, err := c.n.Get(tool)
 		if err != nil {
 			panic(err)
 		}
 
+		logging.Debugf("FileCache: Caching %s", tool.String())
 		err = c.writeDataToCache(layer, cacheKey)
 		if err != nil {
 			panic(err)
 		}
 	}
 
+	logging.Debugf("FileCache: Using cache for %s", tool.String())
 	layer, err := c.readDataFromCache(cacheKey)
 	if err != nil {
 		panic(err)
