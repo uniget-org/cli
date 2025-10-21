65fed1ea1d3ac983716193e45dfdf4f373dde74b
Nicholas Dille <nicholas.dille@mailbox.org>
2025-10-21 17:24:32 +0200

    Read layer from Docker image

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
+	"os"
 
+	"github.com/uniget-org/cli/pkg/archive"
 	"github.com/uniget-org/cli/pkg/containers"
 )
 
@@ -21,7 +24,10 @@ func main() {
 	fmt.Printf("Image %s\n", image)
 	if !containers.CheckDockerImageExists(cli, image) {
 		fmt.Printf("Pulling %s\n", image)
-		containers.PullDockerImage(cli, image)
+		err := containers.PullDockerImage(cli, image)
+		if err != nil {
+			panic(fmt.Errorf("failed to pull docker image: %s", err))
+		}
 	}
 
 	shaString, err := containers.GetFirstLayerShaFromRegistry(toolRef)
@@ -31,24 +37,83 @@ func main() {
 	sha := shaString[7:]
 	fmt.Printf("Layer Sha256: %s\n", sha)
 
+	fmt.Println("Extracting image manifest tar to file foo")
 	err = containers.ReadDockerImage(cli, image, func(reader io.ReadCloser) error {
-		fmt.Println("Reading image")
-		defer reader.Close()
+		file, err := os.Create("foo")
+		if err != nil {
+			return fmt.Errorf("failed to create file: %w", err)
+		}
+		//nolint:errcheck
+		defer file.Close()
+		_, err = io.Copy(file, reader)
+		if err != nil {
+			return fmt.Errorf("failed to copy data: %w", err)
+		}
 
-		fmt.Println("X")
+		return nil
+	})
+	if err != nil {
+		panic(fmt.Errorf("failed to read docker image: %s", err))
+	}
+
+	fmt.Println("Extracting layer tar to file bar")
+	err = containers.ReadDockerImage(cli, image, func(reader io.ReadCloser) error {
 		err := containers.UnpackLayerFromDockerImage(reader, sha, func(reader io.ReadCloser) error {
 			fmt.Println("  Unpacking...")
-			//
-			fmt.Println("  Dome unpacking.")
+
+			file, err := os.Create("bar")
+			if err != nil {
+				return fmt.Errorf("failed to create file: %w", err)
+			}
+			//nolint:errcheck
+			defer file.Close()
+			_, err = io.Copy(file, reader)
+			if err != nil {
+				return fmt.Errorf("failed to copy data: %w", err)
+			}
+
 			return nil
 		})
 		if err != nil {
-			return err
+			return fmt.Errorf("failed to unpack layer: %w", err)
 		}
-		fmt.Println("Y")
-
-		fmt.Println("  Done reading.")
 		return nil
 	})
+	if err != nil {
+		panic(fmt.Errorf("failed to read docker image: %s", err))
+	}
+
+	fmt.Println("Listing items in layer tar")
+	err = containers.ReadDockerImage(cli, image, func(reader io.ReadCloser) error {
+		err := containers.UnpackLayerFromDockerImage(reader, sha, func(reader io.ReadCloser) error {
+			fmt.Println("  Unpacking...")
+
+			buf := new(bytes.Buffer)
+			length, err := buf.ReadFrom(reader)
+			if err != nil {
+				return fmt.Errorf("failed to copy data: %w", err)
+			}
+			fmt.Printf("  Read %d bytes\n", length)
+			byteArray := buf.Bytes()
+			fmt.Printf("  Total layer size: %d bytes\n", len(byteArray))
+
+			fmt.Println("  Processing tar contents...")
+			layerReader := bytes.NewReader(byteArray)
+			err = archive.ProcessTarContents(io.NopCloser(layerReader), archive.CallbackDisplayTarItem)
+			if err != nil {
+				return fmt.Errorf("failed to process tar: %w", err)
+			}
+			fmt.Println("  Done processing tar contents.")
+
+			return nil
+		})
+		if err != nil {
+			return fmt.Errorf("failed to unpack layer: %w", err)
+		}
+		return nil
+	})
+	if err != nil {
+		panic(fmt.Errorf("failed to read docker image: %s", err))
+	}
 
 }
