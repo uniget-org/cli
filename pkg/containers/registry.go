edb9936760fa397995884bf971895feee7055905
Nicholas Dille <nicholas.dille@mailbox.org>
2025-11-25 15:08:30 +0100

    Reverted fix

--- pkg/containers/registry.go
+++ pkg/containers/registry.go
@@ -23,7 +23,6 @@ import (
 
 func GetRegclient() *regclient.RegClient {
 	rcOpts := []regclient.Opt{}
-	rcOpts = append(rcOpts, regclient.WithDockerCredsFile(""))
 	rcOpts = append(rcOpts, regclient.WithUserAgent("uniget"))
 	rcOpts = append(rcOpts, regclient.WithConfigHost(config.Host{
 		Name: "127.0.0.1:5000",
