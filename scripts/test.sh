45e09cb3d2675c31dd24f980b0c8b347c637fdd1
Nicholas Dille <nicholas.dille@mailbox.org>
2026-02-09 16:08:24 +0100

    fix: Enabled all CLI tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -44,39 +44,39 @@ echo "Testing download of metadata"
 uniget --prefix=${TEMP_DIR} update
 check_file "${TEMP_DIR}/var/cache/uniget/metadata.json" "Metadata" || exit 1
 
-#echo "-----------------------------"
-#echo "Testing install and uninstall of gojq"
-#uniget --prefix=${TEMP_DIR} install gojq
-#check_dir "${TEMP_DIR}/var/cache/uniget/gojq" "Marker file" || exit 1
-#check_file "${TEMP_DIR}/var/lib/uniget/manifests/gojq.json" "Manifest" || exit 1
-#check_file "${TEMP_DIR}/var/lib/uniget/manifests/gojq.txt" "File list" || exit 1
-#"${TEMP_DIR}/usr/local/bin/gojq" --version || exit 1
-#uniget --prefix=${TEMP_DIR} uninstall gojq
-#check_dir "${TEMP_DIR}/var/cache/uniget/gojq" "Marker file" && exit 1
-#check_file "${TEMP_DIR}/var/lib/uniget/manifests/gojq.json" "Manifest" && exit 1
-#check_file "${TEMP_DIR}/var/lib/uniget/manifests/gojq.txt" "File list" && exit 1
+echo "-----------------------------"
+echo "Testing install and uninstall of gojq"
+uniget --prefix=${TEMP_DIR} install gojq
+check_dir "${TEMP_DIR}/var/cache/uniget/gojq" "Marker file" || exit 1
+check_file "${TEMP_DIR}/var/lib/uniget/manifests/gojq.json" "Manifest" || exit 1
+check_file "${TEMP_DIR}/var/lib/uniget/manifests/gojq.txt" "File list" || exit 1
+"${TEMP_DIR}/usr/local/bin/gojq" --version || exit 1
+uniget --prefix=${TEMP_DIR} uninstall gojq
+check_dir "${TEMP_DIR}/var/cache/uniget/gojq" "Marker file" && exit 1
+check_file "${TEMP_DIR}/var/lib/uniget/manifests/gojq.json" "Manifest" && exit 1
+check_file "${TEMP_DIR}/var/lib/uniget/manifests/gojq.txt" "File list" && exit 1
 
-#echo "-----------------------------"
-#echo "Testing install of yq"
-#uniget --prefix=${TEMP_DIR} --target=usr install yq
-#check_dir "${TEMP_DIR}/var/cache/uniget/yq" "Marker file" || exit 1
-#check_file "${TEMP_DIR}/var/lib/uniget/manifests/yq.json" "Manifest" || exit 1
-#check_file "${TEMP_DIR}/var/lib/uniget/manifests/yq.txt" "File list" || exit 1
-#uniget --prefix=${TEMP_DIR} --target=usr version yq || exit 1
-#
-#echo "-----------------------------"
-#echo "Testing inspection of jq"
-#uniget inspect jq | grep "bin/jq$" || exit 1
+echo "-----------------------------"
+echo "Testing install of yq"
+uniget --prefix=${TEMP_DIR} --target=usr install yq
+check_dir "${TEMP_DIR}/var/cache/uniget/yq" "Marker file" || exit 1
+check_file "${TEMP_DIR}/var/lib/uniget/manifests/yq.json" "Manifest" || exit 1
+check_file "${TEMP_DIR}/var/lib/uniget/manifests/yq.txt" "File list" || exit 1
+uniget --prefix=${TEMP_DIR} --target=usr version yq || exit 1
 
-#echo "-----------------------------"
-#echo "Testing install in user context"
-#uniget --user update
-#test -f "${HOME}/.cache/uniget/metadata.json"
-#uniget --user install gojq
-#test -d "${HOME}/.cache/uniget/gojq"
-#test -f "${HOME}/.local/state/uniget/manifests/gojq.json"
-#test -f "${HOME}/.local/state/uniget/manifests/gojq.txt"
-#uniget --user uninstall gojq
+echo "-----------------------------"
+echo "Testing inspection of jq"
+uniget inspect jq | grep "bin/jq$" || exit 1
+
+echo "-----------------------------"
+echo "Testing install in user context"
+uniget --user update
+test -f "${HOME}/.cache/uniget/metadata.json"
+uniget --user install gojq
+test -d "${HOME}/.cache/uniget/gojq"
+test -f "${HOME}/.local/state/uniget/manifests/gojq.json"
+test -f "${HOME}/.local/state/uniget/manifests/gojq.txt"
+uniget --user uninstall gojq
 
 echo "-----------------------------"
 echo "Testing hooks"
